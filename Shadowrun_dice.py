a3f8bdc5a635b68f6f2022ca14865730de30d5a5
JosephineVanpool <Josephine.Vanpool@gmail.com>
2021-06-05 13:31:07 -0500

    cleaned up and provided clarity

--- Shadowrun_dice.py
+++ Shadowrun_dice.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
-#import random
 from random import randint
-
 
 class Shadowrun_Dice ():
     def initialRoll():
         #ask for the number of dice and set initial values
         numDice = int(input ("How many 6-sided dice do you need to roll? "))
         numSixes = 0
-        initialDiceRolled = [] #inital rolling of the dice list
+        initialDiceRolled = [] #inital list for rolling of the dice
   
         #iterate through each die and generate an integer between 1-6 ("rolling the dice")
         #add the generated value to the list, and increment the number of sixes by 1 if the number generated is a 6
@@ -21,26 +19,23 @@ class Shadowrun_Dice ():
         return initialDiceRolled,numSixes
 
 
-    def reRollSixes(sixes):
-        rerollSixes = [] #list of 6's that were re-rolled
+    def RollSixes(sixes):
         numSixes=sixes
-        #if there are any sixes generated during the inital roll, generate an integer between 1-6 for each 6 and decriment the number of sixes by 1
-        # if there is another six that gets generated, increment the number of sixes by 1
-        #loop until there are no more sixes left to generate new die for
+        rerollSixes = [] #empty list of 6's that were re-rolled
+
+        #if sixes are generated during the inital roll, generate an integer between 1-6 for each 6 and decriment the number of sixes by 1
+        # if there is a six that gets generated, increment the number of sixes by 1, and continue until no more sixes
         while numSixes != 0:
             value = randint(1, 6)
             rerollSixes.append(value) 
             numSixes -= 1
             if value == 6:
                 numSixes += 1
-            return rerollSixes
-   
-
-
-        
-    diceRolled, sixes=initialRoll()
-    RollSixes= reRollSixes(sixes)
+        return rerollSixes
+           
+    diceRolled, numSixes=initialRoll()
+    reRollSixes= RollSixes(numSixes)
 
     #print the results
     print (f"Results of intial throw: {diceRolled}")
-    print (f"Results of re-rolling sixes: {RollSixes}")
+    print (f"Results of re-rolling sixes: {reRollSixes}")
